e420ce3bd1a500deafdc41e24dcf267cac9e09fb
Rohantunikipati <rohantunikipat@gamil.com>
2024-03-12 22:19:03 +0530

    Connected to MongoDB compass

--- server/index.mjs
+++ server/index.mjs
@@ -1,10 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to database:", err.message);
+  });
 
 const app = express();
-const port =  3000;
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
-app.listen(port,()=>{
-    console.log(`Sever is Running at port ${port}`);
-})
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
