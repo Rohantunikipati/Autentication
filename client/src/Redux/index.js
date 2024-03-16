3199b910fb5a245b3e89d691bb6673ce3065c05a
Rohantunikipati <rohantunikipat@gamil.com>
2024-03-16 16:21:31 +0530

    Added Redux Persist

--- client/src/Redux/index.js
+++ client/src/Redux/index.js
@@ -1,4 +1,6 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, createSlice } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {persistReducer,persistStore} from "redux-persist";
 
 const userSlice = createSlice({
   name: "user",
@@ -9,26 +11,32 @@ const userSlice = createSlice({
   },
   reducers: {
     signInStart: (state, action) => {
-      state.current_user=null,
-      state.loading = true;
+      (state.current_user = null), (state.loading = true);
     },
     signInSuccess: (state, action) => {
-        console.log(action);
-      (state.current_user = action.payload),
-        (state.loading = false)
+      console.log(action);
+      (state.current_user = action.payload), (state.loading = false);
     },
     signInFail: (state, action) => {
-      (state.error = action.payload),
-       (state.loading = false);
+      (state.error = action.payload), (state.loading = false);
     },
   },
 });
+
+const rootReducer = combineReducers({ user: userSlice.reducer });
+
+const presistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(presistConfig, rootReducer);
+
 const Store = configureStore({
-  reducer: {
-    user: userSlice.reducer,
-  },
+  reducer: persistedReducer
 });
 
 export const user_actions = userSlice.actions;
-
+export const persistor =  persistStore(Store);
 export default Store;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import Store from "./Redux/index.js";
+import Store, { persistor } from "./Redux/index.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={Store}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
